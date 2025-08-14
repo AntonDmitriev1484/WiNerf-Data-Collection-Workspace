8a2ed2cea681058f18f49d310853432816c18505
AntonDmitriev1484 <a.dmitriev@wustl.edu>
2025-08-14 15:04:27 -0500

    Push with reasonable coord frames but incorrect vectors, about to remove ros2 dependency

--- post/post_process.py
+++ post/post_process.py
@@ -32,14 +32,14 @@ import pickle
 # Example usage:
 # python3 post_process.py -t stereoi_sq -c cam_target_daslab -a pilot3/anchors.json -p pilot3/apriltags.json -i 10
 
+# python3 post_process.py -t winerf_prelim2 -c cam_target_daslab --crop_start 186 --override_april_start='[0,0.25,0.2535]' --in_tx_location='[0,3,0.57]' --plot_world True
 
 parser = argparse.ArgumentParser(description="Stream collector")
 parser.add_argument("--trial_name" , "-t", type=str)
 parser.add_argument("--cam_calibration_file", "-c", type=str)
-parser.add_argument("--crop_start", type=float) # Pass the ROS timestamp that you want to crop away all data before. Data will still be used to compute transforms.
+parser.add_argument("--crop_start", type=float) # From now on this will be relative time instead of an absolute timestamp
 parser.add_argument("--override_april_start", type=str )
-parser.add_argument("--in_router_data", type=str) # Absolute file path of timestamped AoA CSV file.
-parser.add_argument("--in_tx_data", type=str)
+parser.add_argument("--in_tx_location", type=str )
 parser.add_argument("--plot_world", default=False, type=bool) # Generate a plot of router positions and aoa vectors in world frame when done?
 
 args = parser.parse_args()
@@ -55,11 +55,11 @@ os.makedirs(out_infra1, exist_ok=True)
 os.makedirs(out_infra2, exist_ok=True)
 os.makedirs(out_slam, exist_ok=True)
 
-in_router_data = args.in_router_data
+in_router_data = f'../router/{args.trial_name}.npz'
 in_slam = f'../orbslam/out/{args.trial_name}_cam_traj.txt'
 in_slam_kf = f'../orbslam/out/{args.trial_name}_kf_traj.txt'
 in_kalibr = f"../kalibr/camimu_out/{args.cam_calibration_file}-camchain-imucam.yaml"
-in_tx = f"../world/{args.in_tx_data}"
+in_tx = f"../world/{args.in_tx_location}"
 
 bagpath = Path(f'../collect/ros2/{args.trial_name}')
 
@@ -72,8 +72,9 @@ router_data = np.load(in_router_data)
 print(f" {router_data.files=}")
 
 t_router = router_data['timestamps']
-csi_data = router_data['csi_data']
+csi_data = router_data['csi_matrix']
 aoa_rx_frame = router_data['aoa_matrix']
+signal_strength = router_data['strength']
 
 
 # Need to maintain another array that we can buffer data to before dumping one sensor per csv
@@ -113,6 +114,8 @@ with AnyReader([bagpath], default_typestore=rostypes) as reader:
 # Filter for messages within bag timestamp range.
 START = reader.start_time * 1e-9
 END = reader.end_time * 1e-9
+args.crop_start += START
+
 print(f"ROS duration {START} - {END}")
 print(f"Data start {START} cropped to {args.crop_start}")
 
@@ -132,14 +135,17 @@ def filtt2(arr): # For filtering a CSV output
 Transforms = SimpleNamespace()
 
 
-Transforms.T_cam1_to_rx = np.array([]) # TODO
+Transforms.T_cam1_to_rx = np.eye(4)
+Transforms.T_cam1_to_rx[:3, 3] = np.array([0,-0.089, -0.1525])
+Transforms.T_cam1_to_rx[:3,:3] = np.linalg.inv(np.array([[0,0,1], [-1,0,0], [0,-1,0]]))
+# Transforms.T_cam1_to_rx[:3,:3] = np.array([[0,0,1], [-1,0,0], [0,-1,0]])
 Transforms.T_body_to_cam1 = np.linalg.inv(Transforms.T_cam1_to_rx)
 
 infra1_raw_frames = topic_to_processing['/camera/camera/infra1/image_rect_raw'][1]
 # Transforms = extract_apriltag_pose(slam_data, infra1_raw_frames, Transforms, in_kalibr, in_apriltags)
 # Transforms = extract_apriltag_pose_PnP(slam_data, infra1_raw_frames, Transforms, in_kalibr, in_apriltags)
 
-if args.override_april_start is not None:
+if args.override_april_start is None:
     print(" Need to set manual starting point for this dataset collection!")
     print(" Sorry :(")
     exit()
@@ -153,69 +159,70 @@ R_prior = np.array([[1, 0 ,0 ],
                     [0, -1, 0]], dtype=np.float64)
 T_prior[:3,:3] = R_prior
 T_prior[:3,3] = np.array(json.loads(args.override_april_start))
+Transforms.T_world_to_sorigin = np.eye(4)
 Transforms.T_world_to_sorigin[:3, 3] = np.array(json.loads(args.override_april_start))
+Transforms.T_world_to_sorigin[:3,:3] = R_prior
 best_Z = Transforms.T_world_to_sorigin[2,3]
 
-def rotate_about_world_x(T, theta):
-    # Rotate to world frame, then rotate by the adjustment along world frame x axis.
-    R_about_world_x = np.array([[1, 0, 0, 0], 
-                                [0, np.cos(theta), -np.sin(theta), 0],
-                                [0, np.sin(theta), np.cos(theta), 0],
-                                [0,0,0,1]], dtype=np.float64)
-    T_ = T.copy()
-    T_ =  R_about_world_x @ T
-    return T_
+# def rotate_about_world_x(T, theta):
+#     # Rotate to world frame, then rotate by the adjustment along world frame x axis.
+#     R_about_world_x = np.array([[1, 0, 0, 0], 
+#                                 [0, np.cos(theta), -np.sin(theta), 0],
+#                                 [0, np.sin(theta), np.cos(theta), 0],
+#                                 [0,0,0,1]], dtype=np.float64)
+#     T_ = T.copy()
+#     T_ =  R_about_world_x @ T
+#     return T_
 
-def minimize_for_world_pose(slam_data, best_Z, T_world_to_sorigin, Transforms):
+# def minimize_for_world_pose(slam_data, best_Z, T_world_to_sorigin, Transforms):
 
 
-    def get_T_world_to_body(T_sorigin_to_sbody): # A function because I re-use this a lot
-        T_world_to_body = (
-                        T_world_to_sorigin
-                        @ T_sorigin_to_sbody 
-                        @ np.linalg.inv(Transforms.T_imu_to_sbody) 
-                        @ np.linalg.inv(Transforms.T_body_to_imu)
-        )
-        return T_world_to_body
+#     def get_T_world_to_body(T_sorigin_to_sbody): # A function because I re-use this a lot
+#         T_world_to_body = (
+#                         Transforms.T_world_to_sorigin 
+#                         @ T_sorigin_to_sbody 
+#                         @ Transforms.T_cam1_to_rx
+#         )
+#         return T_world_to_body
 
-    body_poses_world_frame = []
-    zs = []
-    for i in range(slam_data.shape[0]-1):
+#     body_poses_world_frame = []
+#     zs = []
+#     for i in range(slam_data.shape[0]-1):
 
-        T_sorigin_to_sbody = slam_quat_to_HTM(slam_data[i,:])
-        T_world_to_body = get_T_world_to_body(T_sorigin_to_sbody)
-        body_poses_world_frame.append( T_world_to_body )
-        zs.append(T_world_to_body[2,3])
+#         T_sorigin_to_sbody = slam_quat_to_HTM(slam_data[i,:])
+#         T_world_to_body = get_T_world_to_body(T_sorigin_to_sbody)
+#         body_poses_world_frame.append( T_world_to_body )
+#         zs.append(T_world_to_body[2,3])
 
-    # The best (lowest) score has the lowest total error in each poses Z-coordinate
-    # score = sum([ abs( best_Z - T_world_to_body[2,3]) ** 2 for T_world_to_body in body_poses_world_frame])
-    score = np.var(np.abs(np.array(zs)-best_Z))
+#     # The best (lowest) score has the lowest total error in each poses Z-coordinate
+#     # score = sum([ abs( best_Z - T_world_to_body[2,3]) ** 2 for T_world_to_body in body_poses_world_frame])
+#     score = np.var(np.abs(np.array(zs)-best_Z))
 
-    return score
+#     return score
 
-def distance_to_XY_plane_loss(theta):
-    print(f" Trying theta: {theta * 180/np.pi}")
+# def distance_to_XY_plane_loss(theta):
+#     print(f" Trying theta: {theta * 180/np.pi}")
 
-    T_world_to_sorigin = np.eye(4)
-    T_world_to_sorigin[:3,:3] = R_prior # Im pretty sure this is the right order for python
-    T_world_to_sorigin[:3,3] = Transforms.T_world_to_sorigin[:3, 3]
+#     T_world_to_sorigin = np.eye(4)
+#     T_world_to_sorigin[:3,:3] = R_prior # Im pretty sure this is the right order for python
+#     T_world_to_sorigin[:3,3] = Transforms.T_world_to_sorigin[:3, 3]
 
-    T_world_to_sorigin = rotate_about_world_x(T_world_to_sorigin, float(theta[0]))
+#     T_world_to_sorigin = rotate_about_world_x(T_world_to_sorigin, float(theta[0]))
 
-    # Make sure you deep copy slam data every time
-    score = minimize_for_world_pose(slam_data.copy(), best_Z, T_world_to_sorigin, Transforms)
-    print(f" Score {score}")
-    return score
+#     # Make sure you deep copy slam data every time
+#     score = minimize_for_world_pose(slam_data.copy(), best_Z, T_world_to_sorigin, Transforms)
+#     print(f" Score {score}")
+#     return score
 
-INITIAL_THETA = np.array([0]) # Initial guess on theta is 0 rad
-result = minimize(distance_to_XY_plane_loss, INITIAL_THETA, method = 'Nelder-Mead')
-result_theta = result.x[0] # Result has a bizzare projection instead of rotation effect when included in pose chain?
-# Didn't really have time to debug this.
-print(f" Selected Theta: {result.x[0] * 180/np.pi}")
-print("Final loss:", result.fun)
-print(f" Initial Rotation {R_prior}")
-Transforms.T_world_to_sorigin = rotate_about_world_x(T_prior, result_theta)
-print(f" Final rotation { Transforms.T_world_to_sorigin[:3, :3]}")
+# INITIAL_THETA = np.array([0]) # Initial guess on theta is 0 rad
+# result = minimize(distance_to_XY_plane_loss, INITIAL_THETA, method = 'Nelder-Mead')
+# result_theta = result.x[0] # Result has a bizzare projection instead of rotation effect when included in pose chain?
+# # Didn't really have time to debug this.
+# print(f" Selected Theta: {result.x[0] * 180/np.pi}")
+# print("Final loss:", result.fun)
+# print(f" Initial Rotation {R_prior}")
+# Transforms.T_world_to_sorigin = rotate_about_world_x(T_prior, result_theta)
+# print(f" Final rotation { Transforms.T_world_to_sorigin[:3, :3]}")
 
 
 # T_world_to_body = T_cam1_to_rx x T_sorigin_to_sbody x T_world_to_sorigin
@@ -287,11 +294,6 @@ for i in range(t_router.shape[0]-1):
     body_poses_world_frame.append(world_frame_pose)
 
 
-
-with open(f'{out_slam}/body_poses_world_frame.csv', 'w') as fs: csv.writer(fs).writerows(filtt2(body_poses_world_frame))
-with open(f'{out_slam}/slam_poses_slam_frame.csv', 'w') as fs: csv.writer(fs).writerows(filtt2(slam_poses_slam_frame))
-
-
 ### Now convert all AoA vectors to the world frame
 
 all_aoa_vectors_world_frame = []
@@ -299,7 +301,7 @@ all_aoa_vectors_world_frame_rotation = []
 
 for i in range(t_router.shape[0]-1):
 
-    aoa_vectors_rx_frame = aoa_rx_frame[i, :] # Should be an array of 5-6 paths x 3
+    aoa_vectors_rx_frame = aoa_rx_frame[i, :] # Should be an array of 3 paths x 3
     n_paths = aoa_vectors_rx_frame.shape[0]
 
     aoa_vectors_world_frame = np.empty_like(aoa_vectors_rx_frame)
@@ -311,16 +313,16 @@ for i in range(t_router.shape[0]-1):
     # This computation takes the 'tip' of the AoA unit vector in rx frame coordinates
     # And maps it to the 'tip' of the AoA unit vector in world frame coordinates.
     # This means that this will return points w.r.t world origin, and not w.r.t rx frame origin in world frame.
-    for path_idx in n_paths:
-        aoa_vectors_world_frame[path_idx,:] = T_rx_to_world @ np.hstack(aoa_vectors_rx_frame[path_idx, :], [1])
+    for path_idx in range(n_paths):
+        aoa_vectors_world_frame[path_idx,:] = (T_rx_to_world @ np.append(aoa_vectors_rx_frame[path_idx, :], 1))[:3]
     all_aoa_vectors_world_frame.append(aoa_vectors_world_frame)
 
     # This computation rotates the AoA unit vector from rx frame coordinates into world frame coordinates.
     # This means that the tip of this unit vector will be w.r.t 0,0,0, but its rotation will be
     # consistent with the world axes.
     aoa_vectors_world_frame_rotation = np.empty_like(aoa_vectors_rx_frame)
-    for path_idx in n_paths:
-        aoa_vectors_world_frame_rotation[path_idx, :] = T_rx_to_world[:3,3] @ aoa_vectors_rx_frame[path_idx, :]
+    for path_idx in range(n_paths):
+        aoa_vectors_world_frame_rotation[path_idx, :] = np.linalg.inv(T_rx_to_world[:3,:3]) @ aoa_vectors_rx_frame[path_idx, :]
     all_aoa_vectors_world_frame_rotation.append(aoa_vectors_world_frame_rotation)
     # These will provide the RPY of the vector along world frame axes, as measured at the receiver's origin.
 
@@ -336,20 +338,23 @@ for aoa_vector in all_aoa_vectors_world_frame_rotation:
 np.savez(
         outpath+"/roomba_data_world.npz", 
         timestamps = router_data['timestamps'], 
-        csi_data = router_data['csi_data'],
+        csi_data = router_data['csi_matrix'],
         aoa_matrix = router_data['aoa_matrix'],
+        strength = router_data['strength'],
         aoa_matrix_world=aoa_vectors_world, 
         positions_world=positions_world
         )
 
 # Plot results
 
-body_orientation_stride = 50
-aoa_vector_stride = 10
+body_orientation_stride = 100
+aoa_vector_stride = 100
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
+positions_world = np.array(positions_world)
+aoa_vectors_world = np.array(aoa_vectors_world)
 ax.plot(positions_world[:, 0], positions_world[:, 1], positions_world[:, 2], label='Trajectory', color='blue')
 ax.scatter(*positions_world[0], color='green', label='Start')
 ax.scatter(*positions_world[-1], color='red', label='End')
@@ -369,25 +374,14 @@ if body_orientation_stride > 0:
     for i in range(0, len(body_poses_world_frame), body_orientation_stride):
         draw_axes(ax, body_poses_world_frame[i], length=0.4)
 
-def draw_vector(ax, T, length=0.1):
-    """Draw coordinate axes from transformation matrix T."""
-    origin = T[:3, 3]
-    x_axis = T[:3, 0] * length
-    y_axis = T[:3, 1] * length
-    z_axis = T[:3, 2] * length
-
-    ax.quiver(*origin, *x_axis, color='r', length=length, normalize=False)
-    ax.quiver(*origin, *y_axis, color='g', length=length, normalize=False)
-    ax.quiver(*origin, *z_axis, color='b', length=length, normalize=False)
-
 if aoa_vector_stride > 0:
-    length = 0.5
+    length = 0.2
     n_vectors = 1 # Plot first N paths
     for i in range(0, len(positions_world), aoa_vector_stride):
         for j in range(n_vectors):
             origin = positions_world[i]
-            tip = positions_world[i] + aoa_vectors_world[i][j,:]
-            ax.quiver(*origin, *tip, color='purple', length=length, normalize=False)
+            tip = origin + aoa_vectors_world[i][j,:]
+            ax.quiver(*origin, *tip, color='purple')
 
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
@@ -395,6 +389,9 @@ ax.set_zlabel("Z")
 ax.set_title("Trajectory and Static Coordinate Frames")
 ax.view_init(elev=20, azim=45)
 ax.legend()
+ax.set_xlim(-1, 3)
+ax.set_ylim(0,4)
+ax.set_zlim(0,2)
 plt.show()
 pickle.dump(fig, open(outpath+"/trial_viz.pickle", 'wb'))
 
@@ -423,8 +420,6 @@ class NumpyEncoder(json.JSONEncoder):
 
 
 ### Copy all world information: transforms, anchors, apriltags, to output
-shutil.copy(in_anchors, f'{outpath}/anchors.json')
-shutil.copy(in_apriltags, f'{outpath}/apriltags.json')
 with open(f'{outpath}/transforms.json', 'w') as fs: json.dump(vars(Transforms), fs, cls=NumpyEncoder, indent=1)
 
 
